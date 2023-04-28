5bbc148ab3891ebef998727966f2f4381f91ab00
Adriano Siqueira <adriano.siqueira@animaeducacao.com.br>
2023-04-28 11:33:10 -0300

    Initial Forms Design

--- Estacionamento-Desktop/VeiculoForm.Designer.cs
+++ Estacionamento-Desktop/VeiculoForm.Designer.cs
@@ -28,12 +28,195 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ClienteId = new System.Windows.Forms.TextBox();
+            this.Marca = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.VeiculoMarca = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.VeiculoPlaca = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.VeiculoClienteId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.PlacaTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ClienteId";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Placa";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Marca";
+            // 
+            // ClienteId
+            // 
+            this.ClienteId.Location = new System.Drawing.Point(74, 19);
+            this.ClienteId.Name = "ClienteId";
+            this.ClienteId.Size = new System.Drawing.Size(422, 20);
+            this.ClienteId.TabIndex = 3;
+            // 
+            // Marca
+            // 
+            this.Marca.Location = new System.Drawing.Point(74, 73);
+            this.Marca.Name = "Marca";
+            this.Marca.Size = new System.Drawing.Size(157, 20);
+            this.Marca.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.PlacaTextBox);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.ClienteId);
+            this.groupBox1.Controls.Add(this.Marca);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 161);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Veículo";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(686, 120);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Cadastrar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.VeiculoMarca,
+            this.VeiculoPlaca,
+            this.VeiculoClienteId});
+            this.dataGridView1.Location = new System.Drawing.Point(22, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(748, 139);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            // 
+            // VeiculoMarca
+            // 
+            this.VeiculoMarca.HeaderText = "Marca";
+            this.VeiculoMarca.Name = "VeiculoMarca";
+            // 
+            // VeiculoPlaca
+            // 
+            this.VeiculoPlaca.HeaderText = "Placa";
+            this.VeiculoPlaca.Name = "VeiculoPlaca";
+            // 
+            // VeiculoClienteId
+            // 
+            this.VeiculoClienteId.HeaderText = "ClienteId";
+            this.VeiculoClienteId.Name = "VeiculoClienteId";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 202);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(776, 236);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Lista Veiculos";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(603, 196);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Atualizar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(686, 195);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Voltar";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // PlacaTextBox
+            // 
+            this.PlacaTextBox.Location = new System.Drawing.Point(74, 47);
+            this.PlacaTextBox.Mask = "ABC-0A00";
+            this.PlacaTextBox.Name = "PlacaTextBox";
+            this.PlacaTextBox.Size = new System.Drawing.Size(100, 20);
+            this.PlacaTextBox.TabIndex = 9;
+            this.PlacaTextBox.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
+            // 
+            // VeiculoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "VeiculoForm";
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "VeiculoForm";
+            this.RightToLeftLayout = true;
+            this.Text = "Marca";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ClienteId;
+        private System.Windows.Forms.TextBox Marca;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn VeiculoMarca;
+        private System.Windows.Forms.DataGridViewTextBoxColumn VeiculoPlaca;
+        private System.Windows.Forms.DataGridViewTextBoxColumn VeiculoClienteId;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.MaskedTextBox PlacaTextBox;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button1;
     }
 }
