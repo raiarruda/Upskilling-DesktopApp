ebffd6ce04be005f454476177fa62f81638dc205
Adriano Siqueira <adriano.siqueira@animaeducacao.com.br>
2023-04-28 10:06:56 -0300

    Entidades

---
+++ Entities/Cliente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		Guid Id = Guid.NewGuid();
+		string Nome { get; set; }
+		int Telefone { get; set; }
+	
+	}
+}
---
+++ Entities/Valores.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		float Minuto = 0.50f
+	}
+}
